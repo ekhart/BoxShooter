62b831e84d0a24d57d0975874e9ad2d91975ed5d
Artur Dębkowski <arturdebkowski@gmail.com>
2017-02-06 16:15:36 +0100

    if conditions

--- Assets/Scripts/BasicMover.cs
+++ Assets/Scripts/BasicMover.cs
@@ -6,13 +6,20 @@ public class BasicMover : MonoBehaviour {
 
 	public float spinSpeed = 180.0f;
 	public float motionMagnitude = 0.1f;
+
+	public bool doSpin = true;
+	public bool doMotion = false;
 	
 	// Update is called once per frame
 	void Update () {
-		// rotate around the up axis of the gameObject
-		gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+		if (doSpin) {
+			// rotate around the up axis of the gameObject
+			gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+		}
 
-		// move up and down over time
-		gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+		if (doMotion) {
+			// move up and down over time
+			gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+		}		
 	}
 }

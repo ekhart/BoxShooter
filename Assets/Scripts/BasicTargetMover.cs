d2c8a4272a1a9311a1190b725bd308a38c2b3f57
Artur Dębkowski <arturdebkowski@gmail.com>
2017-02-06 16:18:57 +0100

    rename basicmover

--- Assets/Scripts/BasicTargetMover.cs
+++ Assets/Scripts/BasicTargetMover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicMover : MonoBehaviour {
+public class BasicTargetMover : MonoBehaviour {
 
 	public float spinSpeed = 180.0f;
 	public float motionMagnitude = 0.1f;
